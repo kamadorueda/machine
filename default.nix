f6e95bdaad10d54675d112f82c602f949c868835
Kevin Amado <kamadorueda@gmail.com>
2021-03-27 19:42:06 -0500

    feat(conf): use product vscode

--- default.nix
+++ default.nix
@@ -16,56 +16,18 @@ let
       config.allowUnfree = true;
     };
 
+  product = remoteImport {
+    url = "https://gitlab.com/fluidattacks/product/-/archive/4bf8cfc844d927e5904f039a060309acf4f780ea.tar.gz";
+    sha256 = "1zh29gb5ls25399dcr4g310gvlg3b1ngh35lbd0592wms0vjc5jg";
+  };
+
   base = with nixpkgs; [
     curl
     jq
     git
     google-chrome
+    product.makes-dev-vscode
     (writeTextDir "etc/profile.d/bashrc" (builtins.readFile ./bashrc.sh))
-    (vscode-with-extensions.override {
-      vscodeExtensions = nixpkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "bash-ide-vscode";
-          publisher = "mads-hartmann";
-          sha256 = "1hq41fy2v1grjrw77mbs9k6ps6gncwlydm03ipawjnsinxc9rdkp";
-          version = "1.11.0";
-        }
-        {
-          name = "gitlens";
-          publisher = "eamodio";
-          sha256 = "1ba72sr7mv9c0xzlqlxbv1x8p6jjvdjkkf7dn174v8b8345164v6";
-          version = "11.2.1";
-        }
-        {
-          name = "haskell";
-          publisher = "haskell";
-          sha256 = "0vxsn4s27n1aqp5pp4cipv804c9cwd7d9677chxl0v18j8bf7zly";
-          version = "1.2.0";
-        }
-        {
-          name = "language-haskell";
-          publisher = "justusadam";
-          sha256 = "0ab7m5jzxakjxaiwmg0jcck53vnn183589bbxh3iiylkpicrv67y";
-          version = "3.4.0";
-        }
-        {
-          name = "vscode-pylance";
-          publisher = "ms-python";
-          sha256 = "07zapnindwi79k5a2v5ywgwfiqzgs79li73y56rpq0n3a287z4q6";
-          version = "2021.2.3";
-        }
-        {
-          name = "terraform";
-          publisher = "4ops";
-          sha256 = "196026a89pizj8p0hqdgkyllj2spx2qwpynsaqjq17s8v15vk5dg";
-          version = "0.2.1";
-        }
-      ] ++ [
-        nixpkgs.vscode-extensions.bbenoist.Nix
-        nixpkgs.vscode-extensions.ms-azuretools.vscode-docker
-        nixpkgs.vscode-extensions.ms-python.python
-      ];
-    })
   ];
 
   extra = with nixpkgs; [
