2c82bfb35ed15ebb53d32ba818bbcc7dbb8dfdc9
Kevin Amado <kamadorueda@gmail.com>
2021-05-20 23:52:11 -0500

    refac(conf): add sources.nix

--- default.nix
+++ default.nix
@@ -1,20 +1,8 @@
 { flavor ? "all"
 }:
 let
-  # Host functions
-  fetchzip = (import <nixpkgs> { }).fetchzip;
-
   # Sources
-  sources = {
-    nixpkgs = fetchzip {
-      url = "https://github.com/nixos/nixpkgs/archive/932941b79c3dbbef2de9440e1631dfec43956261.tar.gz";
-      sha256 = "F5+ESAMGMumeYuBx7qi9YnE9aeRhEE9JTjtvTb30lrQ=";
-    };
-    product = fetchzip {
-      url = "https://gitlab.com/fluidattacks/product/-/archive/41aa1c5caf9e4122ffbf9690cb14a552ce3f7b23.tar.gz";
-      sha256 = "1rvn9akx4v2mxpnxm99dcmd35il4yjdd856b51mhgzx6cmsqwpk1";
-    };
-  };
+  sources = import ./sources.nix;
 
   # Functions
   directory = dir: src:
@@ -47,6 +35,7 @@ let
     git
     google-chrome
     (writeTextDir "etc/profile.d/bashrc" (builtins.readFile ./bashrc.sh))
+    home-manager
   ]);
 
   extra = ([
@@ -79,6 +68,7 @@ let
     peek
     python38
     sops
+    terraform
     tokei
     tree
     vim
