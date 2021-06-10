932cf57bd4b26695a334bb96cf3b9f4ab0d99c5e
Kevin Amado <kamadorueda@gmail.com>
2021-06-09 21:11:43 -0500

    feat(conf): add empty flake

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "kamadorueda's machine, as code";
+  inputs = {
+    flakeCompat = { url = "github:edolstra/flake-compat"; flake = false; };
+    nixpkgs = { url = "github:nixos/nixpkgs"; };
+  };
+  outputs = attrs: { };
+}
