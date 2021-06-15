36cacf2265ec409a0ea166425feebabe18a82869
Kevin Amado <kamadorueda@gmail.com>
2021-06-14 22:22:53 -0500

    feat(conf): use remote home manager

--- src/sources/default.nix
+++ src/sources/default.nix
@@ -1,3 +1,3 @@
 _: with _; (import ./sources.nix) // {
-  homeManager = /home/kamadorueda/Documents/github/nix-community/home-manager;
+  # homeManager = /home/kamadorueda/Documents/github/nix-community/home-manager;
 }
