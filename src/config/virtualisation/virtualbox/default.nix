90cc0c2a11fa2699ebafe144dd41cf5bb23fe5a6
Kevin Amado <kamadorueda@gmail.com>
2021-06-18 09:05:34 -0500

    feat(conf): add virtualbox

--- src/config/virtualisation/virtualbox/default.nix
+++ src/config/virtualisation/virtualbox/default.nix
@@ -1,13 +1,8 @@
 _: with _; {
   guest = {
-    x11 = true;
     enable = true;
   };
   host = {
     enable = true;
-    headless = false;
-    enableHardening = false;
-    addNetworkInterface = true;
-    enableExtensionPack = true;
   };
 }
