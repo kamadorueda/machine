1de2e54e7cc6b0f43583ebb8db0f7f92516f97e4
Kevin Amado <kamadorueda@gmail.com>
2021-06-19 21:33:41 -0500

    feat(conf): outsource local config

--- src/hardware/default.nix
+++ src/hardware/default.nix
@@ -1,37 +1,3 @@
 _: with _;
 
-{ lib
-, modulesPath
-, ...
-}:
-
-{
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
-
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "nvme"
-    "usb_storage"
-    "usbhid"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/245ba096-6d14-47a1-8ed2-7b46c3989bdc";
-    fsType = "ext4";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/98C8-16CB";
-    fsType = "vfat";
-  };
-
-  swapDevices = [ ];
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-}
+import ./local.nix
