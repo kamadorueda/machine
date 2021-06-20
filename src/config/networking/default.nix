189b3ec24cad96e6c83e23e4cf97d2081a47f66a
Kevin Amado <kamadorueda@gmail.com>
2021-06-20 01:05:44 -0500

    feat(conf): configure internet

--- src/config/networking/default.nix
+++ src/config/networking/default.nix
@@ -1,7 +1,20 @@
-_: with _; {
+_: with _;
+let
+  interface = "wlp0s20f3";
+  # wpa_passphrase ssid psk
+  psk = "fcdffa3be0858cfa7e299c2f2bdfa47f78699a9eae850d5e9fdad27cad9773ca";
+in
+{
   interfaces = {
-    enp0s31f6 = { useDHCP = true; };
-    wlp0s20f3 = { useDHCP = true; };
+    "${interface}" = { useDHCP = true; };
   };
   useDHCP = false;
+  wireless = {
+    enable = true;
+    interfaces = [ interface ];
+    networks = {
+      "24ieusbajwiqueozmxn" = { pskRaw = psk; };
+      "5kasdkemddmnxuxyqo" = { pskRaw = psk; };
+    };
+  };
 }
