2698bef566c0b3da8408a8921471e007eb3d9e2e
Kevin Amado <kamadorueda@gmail.com>
2021-06-24 21:44:00 -0500

    feat(conf): configure google chrome

--- src/config/programs/chromium/default.nix
+++ src/config/programs/chromium/default.nix
@@ -1,13 +1,29 @@
 _: with _; {
-  # defaultSearchProviderSearchURL =
-  #   "https://encrypted.google.com/search?q={searchTerms}&{google:RLZ}{google:originalQueryForSuggestion}{google:assistedQueryStats}{google:searchFieldtrialParameter}{google:searchClient}{google:sourceId}{google:instantExtendedEnabledParameter}ie={inputEncoding}";
-  # defaultSearchProviderSuggestURL =
-  #   "https://encrypted.google.com/complete/search?output=chrome&q={searchTerms}";
   enable = true;
   extensions = [
     "glnpjglilkicbckjpbgcfkogebgllemb" # okta
     "hdokiejnpimakedhajhdlcegeplioahd" # lastpass
   ];
   # https://chromeenterprise.google/policies/
-  extraOpts = { };
+  extraOpts = {
+    BookmarkBarEnabled = true;
+    ManagedBookmarks = [
+      {
+        toplevel_name = "Go";
+      }
+      {
+        name = "okta";
+        url = "https://fluidattacks.okta.com";
+      }
+      {
+        name = "diff";
+        url = "https://gitlab.com/fluidattacks/product/-/compare/master...kamadoatfluid";
+      }
+      {
+        name = "pipelines";
+        url = "https://gitlab.com/fluidattacks/product/-/pipelines?page=1&scope=all&ref=kamadoatfluid";
+      }
+    ];
+    ShowAppsShortcutInBookmarkBar = false;
+  };
 }
