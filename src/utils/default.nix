fac5eb8411edd772f65a37aa897ef00617aa9cc8
Kevin Amado <kamadorueda@gmail.com>
2021-06-24 20:46:51 -0500

    feat(conf): make script function

--- src/utils/default.nix
+++ src/utils/default.nix
@@ -1,6 +1,24 @@
 _: with _; {
+  debug = expr: builtins.trace expr expr;
   remoteImport = { args ? null, source }:
     if args == null
     then import source
     else import source args;
+  script =
+    { env ? { }
+    , path ? [ ]
+    , src
+    }:
+    let
+      export = k: v: "${k}=${packages.nixpkgs.lib.escapeShellArg v}";
+      exports = packages.nixpkgs.lib.mapAttrsToList export (env // {
+        PATH = packages.nixpkgs.lib.makeBinPath (path ++ [
+          packages.nixpkgs.coreutils
+        ]);
+      });
+    in
+    ''
+      ${builtins.concatStringsSep " " exports} \
+        ${packages.nixpkgs.bash}/bin/bash -euo pipefail ${src}
+    '';
 }
