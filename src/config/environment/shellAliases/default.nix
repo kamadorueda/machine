b22c8a733a66ee65e0d2d2434ff8516ebc591d64
Kevin Amado <kamadorueda@gmail.com>
2021-06-29 00:25:11 -0500

    feat(conf): move shell aliases
    - So I can use m for makes

--- src/config/environment/shellAliases/default.nix
+++ src/config/environment/shellAliases/default.nix
@@ -3,6 +3,7 @@ _: with _; {
   bashrc = "${abs.editor} '${abs.machine}/src/config/programs/bash/bashrc.sh'";
   bat = "bat --show-all --theme=ansi";
   c = "git commit --allow-empty";
+  ca = "git commit --amend --no-edit --allow-empty";
   code = "${abs.editor}";
   csv = "column -s, -t";
   cm = "git log -n 1 --format=%s%n%n%b";
@@ -10,7 +11,6 @@ _: with _; {
   f = "git fetch --all";
   graph = "TZ=UTC git rev-list --date=iso-local --pretty='!%H!!%ad!!%cd!!%aN!!%P!' --graph HEAD";
   l = "git log --show-signature";
-  m = "git commit --amend --no-edit --allow-empty";
   melts = "CI=true CI_COMMIT_REF_NAME=master melts";
   nix3 = "${packages.nixpkgs.nixUnstable}/bin/nix --experimental-features 'ca-references flakes nix-command '";
   now = "date --iso-8601=seconds --utc";
