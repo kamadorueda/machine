5a3e3cb40bbafd72afe77b4f5618afd28ba501ac
Kevin Amado <kamadorueda@gmail.com>
2021-09-08 20:32:55 -0500

    feat(conf): add nixpkgs-python

--- src/hardware/local.nix
+++ src/hardware/local.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
