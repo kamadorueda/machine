47cc10edaacd95fb89fe26273a78e201877acd41
Kevin Amado <kamadorueda@gmail.com>
2021-09-27 23:07:55 -0500

    refac(conf): remove unused packages

--- src/config/boot/default.nix
+++ src/config/boot/default.nix
@@ -4,13 +4,13 @@ _: with _; {
     luks = {
       reusePassphrases = true;
     };
-    postDeviceCommands = packages.nixpkgs.lib.mkAfter ''
-      ${packages.nixpkgs.coreutils}/bin/echo wiping root device...
-      ${packages.nixpkgs.coreutils}/bin/mkdir /mnt-root
-      ${packages.nixpkgs.utillinux}/bin/mount /dev/disk/by-label/root /mnt-root
-      ${packages.nixpkgs.coreutils}/bin/rm -fr /mnt-root/*
-      ${packages.nixpkgs.utillinux}/bin/umount /mnt-root
-      ${packages.nixpkgs.coreutils}/bin/sleep 5
+    postDeviceCommands = with packages.nixpkgs; lib.mkAfter ''
+      ${coreutils}/bin/echo wiping root device...
+      ${coreutils}/bin/mkdir /mnt-root
+      ${utillinux}/bin/mount /dev/disk/by-label/root /mnt-root
+      ${coreutils}/bin/rm -fr /mnt-root/*
+      ${utillinux}/bin/umount /mnt-root
+      ${coreutils}/bin/sleep 5
     '';
   };
   loader = {
--- src/config/environment/systemPackages/default.nix
+++ src/config/environment/systemPackages/default.nix
@@ -1,21 +1,9 @@
 _: with _; [
-  packages.nixpkgs.androidsdk_9_0
-  packages.nixpkgs.asciinema
   packages.nixpkgs.awscli
   packages.nixpkgs.bat
-  packages.nixpkgs.bind
-  packages.nixpkgs.binutils
-  packages.nixpkgs.black
-  packages.nixpkgs.burpsuite
-  packages.nixpkgs.cabal-install
-  packages.nixpkgs.cachix
-  packages.nixpkgs.cargo
   packages.nixpkgs.coreutils
   packages.nixpkgs.curl
   packages.nixpkgs.dconf2nix
-  packages.nixpkgs.diction
-  packages.nixpkgs.diffoscope
-  packages.nixpkgs.dmenu
   packages.nixpkgs.gcc
   packages.nixpkgs.ghc
   packages.nixpkgs.gnome.dconf-editor
@@ -28,38 +16,25 @@ _: with _; [
   packages.nixpkgs.just
   packages.nixpkgs.kubectl
   packages.nixpkgs.libreoffice
-  packages.nixpkgs.lsof
-  packages.nixpkgs.maven
-  packages.nixpkgs.mypy
-  packages.nixpkgs.ngrok
   packages.nixpkgs.niv
   packages.nixpkgs.nix-index
   packages.nixpkgs.nix-info
-  packages.nixpkgs.nixos-generators
   packages.nixpkgs.nixpkgs-fmt
-  packages.nixpkgs.nixpkgs-review
   packages.nixpkgs.nodejs
-  packages.nixpkgs.openjdk
-  packages.nixpkgs.optipng
-  packages.nixpkgs.parallel
   packages.nixpkgs.parted
   packages.nixpkgs.patchelf
-  packages.nixpkgs.pcre
   packages.nixpkgs.peek
-  packages.nixpkgs.python38
-  packages.nixpkgs.qemu
+  packages.nixpkgs.python39
   packages.nixpkgs.shadow
   packages.nixpkgs.shfmt
   packages.nixpkgs.sops
   packages.nixpkgs.terraform
   packages.nixpkgs.tokei
-  packages.nixpkgs.traceroute
   packages.nixpkgs.tree
   packages.nixpkgs.vlc
   packages.nixpkgs.vscode
-  packages.nixpkgs.wirelesstools
-  packages.nixpkgs.wpa_supplicant
   packages.nixpkgs.xclip
   packages.nixpkgs.xdotool
   packages.nixpkgs.yq
+  packages.nixpkgs.yj
 ]
