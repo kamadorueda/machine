3efe977b6e57c51c01a5781bab40395f07489e9a
Kevin Amado <kamadorueda@gmail.com>
2021-11-12 22:48:38 -0500

    feat(conf): flake inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    nixpkgsTimedoctor.url = "github:nixos/nixpkgs/7310407d493ee1c7caf38f8181507d7ac9c90eb8";
+
+    homeManager.url = "github:nix-community/home-manager/master";
+    homeManager.inputs.nixpkgs.follows = "nixpkgs";
+
+    makes.url = "github:fluidattacks/makes/main";
+    makes.inputs.nixpkgs.follows = "nixpkgs";
+
+    pythonOnNix.url = "github:on-nix/python/main";
+    pythonOnNix.inputs.makes.follows = "makes";
+    pythonOnNix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: {
