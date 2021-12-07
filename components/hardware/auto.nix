bcffe1372a18b1a03a2bc187906218c0f53409af
Kevin Amado <kamadorueda@gmail.com>
2021-12-07 14:17:52 -0500

    feat: update sources

--- components/hardware/auto.nix
+++ components/hardware/auto.nix
@@ -48,4 +48,5 @@
   swapDevices = [ ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
