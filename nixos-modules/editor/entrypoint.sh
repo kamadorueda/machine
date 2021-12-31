46bdada3d489e97a3f819260a648752ae5d2b2dd
Kevin Amado <kamadorueda@gmail.com>
2021-12-30 19:22:04 -0500

    fix: export variable

--- nixos-modules/editor/entrypoint.sh
+++ nixos-modules/editor/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p "${user_data_dir}/User"
 copy __argSettings__ "${user_data_dir}/User/settings.json"
 
 # Create --extensions-dir
-source __argExtensions__/template extensions
+source __argExtensions__/template export extensions
 for extension in "${extensions[@]}"; do
   __argBin__ --force --install-extension "${extension}"
 done
