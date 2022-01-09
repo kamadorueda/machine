0da5840975d429a6da685b71e4a4ac67a065c3a5
Kevin Amado <kamadorueda@gmail.com>
2022-01-09 12:22:19 -0500

    feat: update sources

--- nixos-modules/hardware/auto-detected.nix
+++ nixos-modules/hardware/auto-detected.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
