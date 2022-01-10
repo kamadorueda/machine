509f89b45c543eb76ab85ebdb49cc3bee8d007f8
Kevin Amado <kamadorueda@gmail.com>
2022-01-09 19:23:23 -0500

    feat: simplify virtualization

--- nixos-modules/virtualisation/default.nix
+++ nixos-modules/virtualisation/default.nix
@@ -5,10 +5,5 @@
 {
   users.groups.docker = { };
   users.users.${config.wellKnown.username}.extraGroups = [ "docker" ];
-  virtualisation.docker.autoPrune.enable = true;
-  virtualisation.docker.autoPrune.dates = "12:00";
-  virtualisation.docker.autoPrune.flags = [ "-a" "-f" "--volumes" ];
   virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.guest.enable = true;
 }
