4080b76f5f8a00be72a30ff6f2b41eae3d978220
Kevin Amado <kamadorueda@gmail.com>
2022-01-10 10:25:12 -0500

    feat: disable erase your darlings
    - Until the weekend where I have some time
      to find a way to manage it automatically

--- nixos-modules/boot/default.nix
+++ nixos-modules/boot/default.nix
@@ -1,13 +1,14 @@
 { nixpkgs, ... }:
 
 {
-  boot.initrd.postDeviceCommands = ''
-    echo wiping root device...
-    mkdir /tmp/root
-    ${nixpkgs.utillinux}/bin/mount /dev/disk/by-label/root /tmp/root
-    rm -fr /tmp/root/*
-    ${nixpkgs.utillinux}/bin/umount /tmp/root
-  '';
+  # I have to manage some state now
+  # boot.initrd.postDeviceCommands = ''
+  #   echo wiping root device...
+  #   mkdir /tmp/root
+  #   ${nixpkgs.utillinux}/bin/mount /dev/disk/by-label/root /tmp/root
+  #   rm -fr /tmp/root/*
+  #   ${nixpkgs.utillinux}/bin/umount /tmp/root
+  # '';
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.extraEntries = ''
