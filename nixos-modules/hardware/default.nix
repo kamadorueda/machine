fd16d7e2f35d3c93cad31168085d6f9069065dbf
Kevin Amado <kamadorueda@gmail.com>
2022-01-15 22:30:31 -0500

    feat: move nvidia stuff to hardware

--- nixos-modules/hardware/default.nix
+++ nixos-modules/hardware/default.nix
@@ -4,5 +4,7 @@ let
   autoDetected = import ./auto-detected.nix args;
 in
 autoDetected // {
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  services.xserver.videoDrivers = [ "nvidia" ];
   swapDevices = [ ];
 }
