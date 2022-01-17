b495afbf0bfb7617e11bf29a425dae237206b32f
Kevin Amado <kamadorueda@gmail.com>
2022-01-16 23:06:09 -0500

    feat: use alacritty
    - After digging around with many terminals
      I came to the conclusion that this one is the best

--- nixos-modules/terminal/default.nix
+++ nixos-modules/terminal/default.nix
@@ -27,10 +27,29 @@
   ];
   home-manager.users.${config.wellKnown.username} = {
     programs.alacritty.enable = true;
+    programs.alacritty.package = nixpkgs.alacritty;
     programs.alacritty.settings = {
+      colors.normal.black = "#000000";
+      colors.normal.red = "#CD0000";
+      colors.normal.green = "#00CD00";
+      colors.normal.yellow = "#0000EE";
+      colors.normal.blue = "#0000ee";
+      colors.normal.magenta = "#CD00CD";
+      colors.normal.cyan = "#00CDCD";
+      colors.normal.white = "#E5E5E5";
+      colors.bright.black = "#7F7F7F";
+      colors.bright.red = "#FF0000";
+      colors.bright.green = "#00FF00";
+      colors.bright.yellow = "#FFFF00";
+      colors.bright.blue = "#5C5CFF";
+      colors.bright.magenta = "#FF00FF";
+      colors.bright.cyan = "#00FFFF";
+      colors.bright.white = "#FFFFFF";
+      colors.primary.background = "#000000";
+      colors.primary.foreground = "#FFFFFF";
       font.normal.family = config.ui.font;
       font.size = config.ui.fontSize;
-      scrolling.history = 1000000;
+      scrolling.history = 100000;
     };
     programs.bash.enable = true;
     programs.bash.initExtra = "test -f /etc/bashrc && source /etc/bashrc";
@@ -40,11 +59,10 @@
     programs.git.extraConfig = {
       commit.gpgsign = true;
       diff.renamelimit = 16384;
-      diff.sopsdiffer.textconv =
-        (nixpkgs.writeScript "sopsdiffer.sh" ''
-          #! ${nixpkgs.bash}/bin/bash
-          sops -d "$1" || cat "$1"
-        '').outPath;
+      diff.sopsdiffer.textconv = (nixpkgs.writeScript "sopsdiffer.sh" ''
+        #! ${nixpkgs.bash}/bin/bash
+        sops -d "$1" || cat "$1"
+      '').outPath;
       gpg.progam = "${nixpkgs.gnupg}/bin/gpg2";
       gpg.sign = true;
       init.defaultbranch = "main";
