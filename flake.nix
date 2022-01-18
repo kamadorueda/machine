9e08c2b6bab7b42bbdc506bc252d2cc7b849284a
Kevin Amado <kamadorueda@gmail.com>
2022-01-18 17:46:21 -0500

    feat: change secrets of location

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
           secrets.hashedPassword =
             # mkpasswd -m sha-512
             "$6$qQYhouD2P24RYK1H$Oc9BI/2wC7uydLXP5taS7LQgpTUbORwty/0sAGtwial7k9ZYQOmeyjZ5DxvmObdccPJHem2N/.afn/JtCJ2af.";
-          secrets.path = "/data/github/kamadorueda/secrets";
+          secrets.path = "/data/secrets";
           ui.timezone = "America/Bogota";
           wellKnown.email = "kamadorueda@gmail.com";
           wellKnown.name = "Kevin Amado";
