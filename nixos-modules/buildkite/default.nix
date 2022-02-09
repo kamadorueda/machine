ffd3c2c7afbdf96c609ba183d9014207224cfc60
Kevin Amado <kamadorueda@gmail.com>
2022-02-08 19:32:09 -0500

    feat: harden buildkite service

--- nixos-modules/buildkite/default.nix
+++ nixos-modules/buildkite/default.nix
@@ -3,38 +3,51 @@
 , ...
 }:
 {
-  services.buildkite-agents.default = {
-    hooks.environment = ''
-      case "$BUILDKITE_PIPELINE_NAME" in
-        alejandra)
-          case "$BUILDKITE_BRANCH" in
-            main)
-              export CACHIX_AUTH_TOKEN="$( \
-                cat ${config.secrets.path}/machine/cachix-auth-token-alejandra)"
-              ;;
-          esac
-          ;;
-      esac
-    '';
-    runtimePackages = [
-      nixpkgs.bash
-      nixpkgs.cachix
-      nixpkgs.direnv
-      nixpkgs.git
-      nixpkgs.gnutar
-      nixpkgs.gzip
-      nixpkgs.nix
-      (
-        nixpkgs.writeShellScriptBin "nix3" ''
-          exec ${nixpkgs.nixUnstable}/bin/nix \
-            --extra-experimental-features nix-command \
-            --extra-experimental-features flakes \
-            --print-build-logs \
-            "$@"
-        ''
-      )
+  containers.buildkite = {
+    autoStart = true;
+    bindMounts."/secrets/buildkite-token" = {
+      hostPath = "${config.secrets.path}/machine/buildkite-token";
+    };
+    bindMounts."/secrets/cachix-auth-token-alejandra" = {
+      hostPath = "${config.secrets.path}/machine/cachix-auth-token-alejandra";
+    };
+    config.services.buildkite-agents.default = {
+      hooks.environment = ''
+        case "$BUILDKITE_PIPELINE_NAME" in
+          alejandra)
+            case "$BUILDKITE_BRANCH" in
+              main)
+                export CACHIX_AUTH_TOKEN="$(cat /secrets/cachix-auth-token-alejandra)"
+                ;;
+            esac
+            ;;
+        esac
+      '';
+      runtimePackages = [
+        nixpkgs.bash
+        nixpkgs.cachix
+        nixpkgs.direnv
+        nixpkgs.git
+        nixpkgs.gnutar
+        nixpkgs.gzip
+        nixpkgs.nix
+        (
+          nixpkgs.writeShellScriptBin "nix3" ''
+            exec ${nixpkgs.nixUnstable}/bin/nix \
+              --extra-experimental-features nix-command \
+              --extra-experimental-features flakes \
+              --print-build-logs \
+              "$@"
+          ''
+        )
+      ];
+      shell = "${nixpkgs.bash}/bin/bash -euo pipefail -c";
+      tokenPath = "/secrets/buildkite-token";
+    };
+    ephemeral = true;
+    extraFlags = [
+      "--private-users=pick"
+      "--private-users-ownership=auto"
     ];
-    shell = "${nixpkgs.bash}/bin/bash -euo pipefail -c";
-    tokenPath = "${config.secrets.path}/machine/buildkite-token";
   };
 }
