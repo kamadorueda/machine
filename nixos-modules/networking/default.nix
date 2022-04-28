9fdcf803b395063d441f10623befe7661219402b
Kevin Amado <kamadorueda@gmail.com>
2022-04-27 23:12:06 -0600

    feat: remove leaked wifi passwords

--- nixos-modules/networking/default.nix
+++ nixos-modules/networking/default.nix
@@ -1,8 +1,13 @@
 {config, ...}: {
-  environment.etc."NetworkManager/system-connections/home50.nmconnection" = {
+  environment.etc."NetworkManager/system-connections/eduroam.nmconnection" = {
     enable = true;
     mode = "0400";
-    source = ./home50.nmconnection;
+    source = "${config.secrets.path}/machine/eduroam.nmconnection";
+  };
+  environment.etc."NetworkManager/system-connections/SPSETUP-2C38.nmconnection" = {
+    enable = true;
+    mode = "0400";
+    source = "${config.secrets.path}/machine/spsetup-2c38.nmconnection";
   };
   networking.networkmanager.enable = true;
   users.users.${config.wellKnown.username}.extraGroups = ["networkmanager"];
