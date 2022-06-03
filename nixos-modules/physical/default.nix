049709fc1b18f1982db5f0cf617c9999cccf36ce
Kevin Amado <kamadorueda@gmail.com>
2022-06-02 21:04:08 -0600

    feat: enable erase your darlings

--- nixos-modules/physical/default.nix
+++ nixos-modules/physical/default.nix
@@ -14,6 +14,14 @@
     boot.loader.grub.efiSupport = true;
     boot.loader.grub.useOSProber = true;
     boot.loader.timeout = 60;
+    boot.initrd.postDeviceCommands = ''
+      echo wiping root device...
+      mkdir /tmp/root
+      ${nixpkgs.utillinux}/bin/mount /dev/disk/by-label/root /tmp/root
+      rm -fr /tmp/root/*
+      ${nixpkgs.utillinux}/bin/umount /tmp/root
+      sleep 10
+    '';
     swapDevices = [
       {
         device = "/swap";
