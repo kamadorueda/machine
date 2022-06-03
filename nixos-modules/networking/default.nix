6c20a77ecf30fa2b45df6812f03fa076f2be77b1
Kevin Amado <kamadorueda@gmail.com>
2022-06-02 21:40:08 -0600

    feat: stateless wifi networks

--- nixos-modules/networking/default.nix
+++ nixos-modules/networking/default.nix
@@ -1,15 +1,15 @@
 {config, ...}: {
-  environment.etc."NetworkManager/system-connections/wifi-airuc-secure" = {
+  environment.etc."NetworkManager/system-connections/wifi-airuc-secure.nmconnection" = {
     enable = true;
     mode = "0400";
     source = "${config.secrets.path}/wifi-airuc-secure";
   };
-  environment.etc."NetworkManager/system-connections/wifi-eduroam" = {
+  environment.etc."NetworkManager/system-connections/wifi-eduroam.nmconnection" = {
     enable = true;
     mode = "0400";
     source = "${config.secrets.path}/wifi-eduroam";
   };
-  environment.etc."NetworkManager/system-connections/wifi-SPSETUP-2C38" = {
+  environment.etc."NetworkManager/system-connections/wifi-spsetup-2c38.nmconnection" = {
     enable = true;
     mode = "0400";
     source = "${config.secrets.path}/wifi-spsetup-2c38";
