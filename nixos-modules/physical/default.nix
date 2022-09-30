94cfa55c967ce4d161f7f298f8c2e248c520ad52
Kevin Amado <kamadorueda@gmail.com>
2022-09-29 21:33:12 -0600

    feat: update inputs

--- nixos-modules/physical/default.nix
+++ nixos-modules/physical/default.nix
@@ -11,9 +11,9 @@
     boot.initrd.postDeviceCommands = ''
       echo wiping root device...
       mkdir /tmp/root
-      ${nixpkgs.utillinux}/bin/mount /dev/disk/by-label/root /tmp/root
+      ${nixpkgs.util-linux}/bin/mount /dev/disk/by-label/root /tmp/root
       rm -fr /tmp/root/*
-      ${nixpkgs.utillinux}/bin/umount /tmp/root
+      ${nixpkgs.util-linux}/bin/umount /tmp/root
     '';
     swapDevices = [
       {
