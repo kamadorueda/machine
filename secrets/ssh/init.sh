2c055a0501e440402ca015879beb5eeb552534a1
Kevin Amado <kamadorueda@gmail.com>
2022-11-17 10:18:15 -0700

    feat: add ssh keys

--- secrets/ssh/init.sh
+++ secrets/ssh/init.sh
@@ -3,15 +3,3 @@
 cd secrets/ssh/
 
 chmod 600 kamadorueda*
-
-if test -n "${WSL_DISTRO_NAME:-}"; then
-  rm -rf ~/.ssh/
-  mkdir -p ~/.ssh/
-  cp kamadorueda* ~/.ssh/ 
-  chmod 600 ~/.ssh/kamadorueda*
-  {
-    echo 'Host github.com'
-    echo '  HostName github.com'
-    echo '  IdentityFile ~/.ssh/kamadorueda'
-  } > ~/.ssh/config
-fi
--- secrets/ssh/rotate.sh
+++ secrets/ssh/rotate.sh
@@ -4,5 +4,8 @@ cd secrets/ssh/
 
 chmod 700 .
 rm kamadorueda*
+rm kevinatholdings*
 ssh-keygen -t ed25519 -C kamadorueda@gmail.com -f kamadorueda
+ssh-keygen -t ed25519 -C kevin@holdings.io -f kevinatholdings
 ssh-add kamadorueda
+ssh-add kevinatholdings
