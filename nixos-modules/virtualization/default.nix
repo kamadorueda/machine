745d6157e946ae72d0553e25257e4e09de320795
Kevin Amado <kamadorueda@gmail.com>
2023-05-03 13:53:15 -0600

    udpate system

--- nixos-modules/virtualization/default.nix
+++ nixos-modules/virtualization/default.nix
@@ -1,13 +1,13 @@
 {config, ...}: {
-  boot.binfmt.emulatedSystems = [
-    "aarch64-linux"
-    "armv6l-linux"
-    "armv7l-linux"
-    "i686-linux"
-    "i686-windows"
-    "mipsel-linux"
-    "x86_64-windows"
-  ];
+  # boot.binfmt.emulatedSystems = [
+  #   "aarch64-linux"
+  #   "armv6l-linux"
+  #   "armv7l-linux"
+  #   "i686-linux"
+  #   "i686-windows"
+  #   "mipsel-linux"
+  #   "x86_64-windows"
+  # ];
   users.groups.docker = {};
   users.users.${config.wellKnown.username}.extraGroups = ["docker" "vboxusers"];
   virtualisation.docker.enable = true;
